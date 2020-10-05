1e99ce2f0e6a9970fbb4232129be4522ba679625
Ngoc Tran <hello@trannn.ml>
2020-10-05 07:15:31 +0700

    temp

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
+target = "";
+del_ = false;
+count = 0;
+
 window.onload = () => {
-    function set_status(text) {
-        if (typeof(text) !== "string") text = "the <span>.ml</span> stands for ...";
-        document.getElementById("portrait_title").innerHTML = text;
+    chooser = document.getElementById("choose");
+    function set_target(text) {
+        if (chooser.innerHTML !== text) {
+            target = text;
+            del_ = true;
+        }
     }
 
-    document.getElementById("top").onmouseenter = () => {
-        set_status("machine learning");
+    top_elem = document.getElementById("top");
+    bot_elem = document.getElementById("bot");
+    
+    function blur(elem, reset) {
+        elem.style.filter = (reset ? "none" : "saturate(50%) blur(4px) grayscale(30%) brightness(60%)");
+    }
+
+    top_elem.onmouseenter = () => {
+        set_target("resume and stuff");
+        blur(bot_elem);
+        
+        // top_elem.style["border-image-source"] = "linear-gradient(to left, #743ad5, #d53a9d)";
+        // bot_elem.style["border-image-source"] = "none";
     };
 
-    document.getElementById("top").onmouseleave = set_status;
+    top_elem.onmouseleave = () => {
+        set_target("Pick one.");
+        blur(bot_elem, true);
+    }
 
-    document.getElementById("top").onclick = () => {
+    top_elem.onclick = () => {
         window.location.href = 'https://hire.kwkt.ml';
     };
 
-    document.getElementById("bot").onmouseenter = () => {
-        set_status("[expletive]");
+    bot_elem.onmouseenter = () => {
+        set_target("in-your-face music");
+        blur(top_elem);
+
+        // top_elem.style["border-image-source"] = "none";
+        // bot_elem.style["border-image-source"] = "linear-gradient(to left, #743ad5, #d53a9d)";
     };
 
-    document.getElementById("bot").onmouseleave = set_status;
+    bot_elem.onmouseleave = () => {
+        set_target("Pick one.");
+        blur(top_elem, true);
+    }
     
-    document.getElementById("bot").onclick = () => {
+    bot_elem.onclick = () => {
         window.location.href = 'https://hear.kwkt.ml';
     };
+
+    kawaii = document.getElementById("kawaii");
+    dot_cycle = 30;
+    (async () => { while (1) {
+        await new Promise(resolve => setTimeout(resolve, 50));
+        if (count % dot_cycle == 0) {
+            kawaii.innerHTML = kawaii.innerHTML.substr(0, 6) + '.'.repeat(count / dot_cycle)};
+        count = (count + 1) % (dot_cycle * 4);
+
+        if (del_) {
+            if (chooser.innerHTML.length > 0) {
+                chooser.innerHTML = chooser.innerHTML.substr(0, chooser.innerHTML.length - 1);
+                continue;
+            }
+            else del_ = false;
+        }
+        else if (target.length == 0) continue;
+        chooser.innerHTML = chooser.innerHTML + target.substr(0, 1);
+        target = target.substr(1);
+    }})();
 }
